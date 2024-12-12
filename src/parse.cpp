c1f447405e09011e19f439046a29af83d6112348
Iacopo Rozzo <iacopo@sysdig.com>
2024-12-12 11:43:55 +0100

    Wire host root

--- src/parse.cpp
+++ src/parse.cpp
@@ -149,9 +149,8 @@ void my_plugin::parse_diff_async_event(const falcosecurity::parse_event_input &i
 		                                                    e,
 		                                                    tid,
 		                                                    proc_entry::proc_file::comm));
-		// Insert in the vector or create a new entry if it does not exist
 		m_context.sinsp_entries.insert({sinsp_comm.tid, {sinsp_comm}});
-		proc_entry proc_comm(proc_entry::from_proc_fs(sinsp_comm));
+		proc_entry proc_comm(proc_entry::from_proc_fs(sinsp_comm, m_cfg.host_root));
 		if(!proc_comm.path.empty()) {
 			m_context.proc_entries.insert({proc_comm.tid, {proc_comm}});
 		}
